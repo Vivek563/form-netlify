e786cbbf1bda9bd1f5e14a5531bb726f9ecd5de5
Vivek Maurya <Vivek563maurya@gmail.com>
2024-07-14 22:04:35 +0530

    fix: form

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,29 @@
 "use client";
-import { useEffect, useState } from "react";
 
-export default function Home() {
-     const [success, setSuccess] = useState(false);
+import React, { useState } from "react";
 
-     useEffect(() => {
-          if (window.location.search.includes("success=true")) {
-               setSuccess(true);
-          }
-     }, []);
+const Form: React.FC = () => {
+     const [name, setName] = useState("");
+     const [email, setEmail] = useState("");
+
+     const handleSubmit = (event: React.FormEvent) => {
+          event.preventDefault();
+     };
+
      return (
-          <>
-               {success && <p style={{ color: "green" }}>Successfully submitted form!</p>}
-               <div className="flex items-center justify-center min-h-screen gap-10 text-black">
-                    <div className="flex gap-10">
-                         <form action="/?success=true" className="flex gap-10 flex-col" name="contact" method="POST" data-netlify="true">
-                              <input type="hidden" name="form-name" value="contact" />
-                              <p>
-                                   <label htmlFor="name">Name</label>
-                                   <input type="text" id="name" name="name" />
-                              </p>
-                              <p>
-                                   <label htmlFor="email">Email</label>
-                                   <input type="text" id="email" name="email" />
-                              </p>
-                              <p>
-                                   <label htmlFor="message">Message</label>
-                                   <textarea id="message" name="message"></textarea>
-                              </p>
-                              <p>
-                                   <button type="submit">Send</button>
-                              </p>
-                         </form>
-                    </div>
+          <form onSubmit={handleSubmit} name="contact" method="POST" data-netlify="true">
+               <input type="hidden" name="form-name" value="contact" />
+               <div>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)} />
                </div>
-          </>
+               <div>
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+               </div>
+               <button type="submit">Submit</button>
+          </form>
      );
-}
+};
+
+export default Form;
